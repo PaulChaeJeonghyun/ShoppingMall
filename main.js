a04c52428d5cec8d21544d388ecd6db985ac97f2
PaulChaeJeonghyun <89195896+PaulChaeJeonghyun@users.noreply.github.com>
2022-06-09 17:38:40 -0400

    js mod..

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function loadItems() {
 //Update the list with the given items
 function displayItems(items) {
    const container = document.querySelector('.items');
-   const html = items.map(item => creatHTMLString(item));
+   // const html = items.map(item => creatHTMLString(item));
    // console.log(html);
    container.innerHTML = items.map(item => creatHTMLString(item)).join('');
 }
@@ -18,10 +18,26 @@ function displayItems(items) {
 function creatHTMLString(item) {
    return `
       <li class="item">
-         <img src="${item.image}" alt="${item.type}" class="item_thumbnail">
+         <img src="${item.image}" alt="${item.type}" class="item_thumbnail" />
          <span class="item_description">${item.gender}, ${item.size}</span>
       </li>
    `;
+}
+
+function onButtonClick(event, items) {
+   const dataset = event.target.dataset;
+   const key = dataset.key;
+   const value = dataset.value;
+   
+   if (key == null || value == null) {
+      return;
+   }
+
+   const filtered = items.filter(item => item[key] === value);
+   // console.log(filtered)
+   displayItems(filtered);
+   // console.log(event.target.dataset.key);
+   // console.log(event.target.dataset.value);
 }
 
 function setEventListeners(items) {
@@ -30,10 +46,11 @@ function setEventListeners(items) {
    logo.addEventListener('click', () => displayItems(items));
    buttons.addEventListener('click', event => onButtonClick(event, items));
 }
+
 //main
 loadItems()
    .then(items => {
    displayItems(items);
-   // setEventListeners(items)
+   setEventListeners(items)
    })
 .catch(console.log);
